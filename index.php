f0ffea79833384d1594e6c9825faf58cba5c52a5
pwadeveloper <sirmudiadavid@gmail.com>
2018-04-11 16:25:44 +0200

    Uploaded project files

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="hngstyle.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  </head>
+    <section id="introduction" class="">
+      <div class="fill-viewport">
+        <div class="cent pad translucent">
+    <h2 class="intern">hotels.ng internship 4</h2>
+    <h1 class="hello">HELLO, I AM Imasuen</h1>
+    <h3 class="look">OH LOOK! IS THAT THE TIME?</h3>
+    <div class="time">
+    <?php  echo date("h:i");?>
+    </div>
+
+  </div>
+  </div>
+  </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   
+  </body>
+</html>
